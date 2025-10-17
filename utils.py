0493bafefaf26fe2dc8ab7d2271039d98bfe897d
Wei <zouwwwei@gmail.com>
2025-10-17 15:00:11 -0400

    add probe llama3.1-8b

--- utils.py
+++ utils.py
@@ -42,37 +42,3 @@ def save_pickle(obj, filename):
     with open(f"{filename}.pkl", 'wb') as f:
         pickle.dump(obj, f)
 
-
-def generate_response(model, tokenizer, example):
-    messages = [
-        {"role": "system", "content": example['instruction']},
-        {"role": "user", "content": example['data_prompt']},
-    ]
-
-    input_ids = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        return_tensors="pt"
-    ).to(model.device)
-
-    terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-
-    outputs = model.generate(
-        input_ids,
-        max_new_tokens=256,
-        eos_token_id=terminators,
-        pad_token_id=tokenizer.eos_token_id,
-        do_sample=False,
-    )
-    response = outputs[0][input_ids.shape[-1]:]
-    return tokenizer.decode(response, skip_special_tokens=True)
-
-def get_responses(model, tokenizer, examples):
-    responses = []
-    for example in examples:
-        response = generate_response(model, tokenizer, example)
-        responses.append(response)
-    return responses
